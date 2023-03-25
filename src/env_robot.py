planning 2D debug fix collision detection using rotation matrix

src/env_robot.py
@@ -26,6 +26,11 @@ def get_quaternion_from_euler(roll, pitch, yaw):
 
     return [qx, qy, qz, qw]
 
+def get_rotation_matrix_from_angle_z(theta):
+    matrix = np.array([[math.cos(theta), -math.sin(theta), 0],[math.sin(theta), math.cos(theta), 0],[0, 0, 1]])
+    return matrix
+
+
 
 class Env_Robot:
     def __init__(self, robot=None, obs=None):
@@ -49,11 +54,12 @@ class Env_Robot:
         if obs is None:
             self.obstacles = []
             self.obstacles_vis = []
-            for i in range(7):
-                obs_i_model = fcl.Box(5, 5, 0)
-                obs_i_t = np.array([15, 15, 0]) * np.random.rand(3) + np.array([2.5, 2.5, 0])
+            for i in range(70):
+                obs_i_model = fcl.Box(1, 1, 0)
+                obs_i_t = np.array([20, 20, 0]) * np.random.rand(3) + np.array([2.5, 2.5, 0])
                 # obs_i_t = np.array([10, 10, 0])
-                self.obstacles_vis.append([5, 5] + list(obs_i_t)[:2] + [0])
+
+                self.obstacles_vis.append([1, 1] + list(obs_i_t)[:2] + [0])
                 obs_i_tf = fcl.Transform(obs_i_t)
                 obs_i = fcl.CollisionObject(obs_i_model, obs_i_tf)
                 self.obstacles.append(obs_i)
@@ -72,14 +78,17 @@ class Env_Robot:
         # print(x, y)
         angle = state.getYaw()
         quaternion = get_quaternion_from_euler(0, 0, angle)
+        matrix = get_rotation_matrix_from_angle_z(angle)
         # set robot's state
         self.robot.setTranslation(t)
-        self.robot.setQuatRotation(quaternion)
+        # self.robot.setQuatRotation(quaternion)
+        self.robot.setRotation(matrix)
 
         valid_flag = True
         for obs in self.obstacles:
             dis = fcl.distance(self.robot, obs)
-            if dis < 0.2:
+            ret = fcl.collide(self.robot, obs)
+            if ret > 0:
                 valid_flag = False
                 break
         return valid_flag

src/visualization.py
@@ -128,7 +128,7 @@ def plot_line_in_fig_2D(fig, start, end, r, color):
     l = compute_length_2D(start, end)
     x_r = end[0] - start[0]
     y_r = end[1] - start[1]
-    cnt = int(l + 0.5) + 1
+    cnt = int(l + 0.5)
     for i in range(cnt):
         x = start[0] + int(i / l * x_r)
         y = start[1] + int(i / l * y_r)
@@ -175,14 +175,14 @@ def plot_rotat_rec_env_2D(rec_env, size, pixel_per_meter):
     :return:
     """
     l = size * pixel_per_meter
-    fig = 255 * np.ones((int(l * 1.2), int(l * 1.2), 3), dtype=np.int8)
+    fig = 255 * np.ones((int(l * 1), int(l * 1), 3), dtype=np.int8)
     # plot rotating rectangle obstacles
     for rec in rec_env:
         # print(rec)
         # attention coordinate transformation from map to img!
         rec_size = [rec[1] * pixel_per_meter, rec[0] * pixel_per_meter]
         center_axis1, center_axis2 = org_to_img(rec[2], rec[3], fig.shape, pixel_per_meter)
-        fig = plot_rotating_rec_2D(fig=fig, rec_size=rec_size, state=[center_axis1, center_axis2, rec[4]], r=5,
+        fig = plot_rotating_rec_2D(fig=fig, rec_size=rec_size, state=[center_axis1, center_axis2, rec[4]], r=2,
                                    color="black")
     return fig
 
@@ -191,12 +191,12 @@ def plot_rotat_rec_start_goal_2D(fig, start, goal, pixel_per_meter):
     # plot start
     rec_size = [start[1] * pixel_per_meter, start[0] * pixel_per_meter]
     center_axis1, center_axis2 = org_to_img(start[2], start[3], fig.shape, pixel_per_meter)
-    fig = plot_rotating_rec_2D(fig=fig, rec_size=rec_size, state=[center_axis1, center_axis2, start[4]], r=5,
+    fig = plot_rotating_rec_2D(fig=fig, rec_size=rec_size, state=[center_axis1, center_axis2, start[4]], r=3,
                                color="blue")
     # plot goal
     rec_size = [goal[1] * pixel_per_meter, goal[0] * pixel_per_meter]
     center_axis1, center_axis2 = org_to_img(goal[2], goal[3], fig.shape, pixel_per_meter)
-    fig = plot_rotating_rec_2D(fig=fig, rec_size=rec_size, state=[center_axis1, center_axis2, goal[4]], r=5,
+    fig = plot_rotating_rec_2D(fig=fig, rec_size=rec_size, state=[center_axis1, center_axis2, goal[4]], r=3,
                                color="red")
 
     return fig