02bcb73d9affaca268b7cc4781db8886a7cea06f
workserver_ubuntu <wyhboos@sjtu.edu.cn>
2023-04-23 10:49:53 +0800

    track generate code

--- src/generate_data.py
+++ src/generate_data.py
@@ -5,37 +5,37 @@ def generate_path_main(args):
     # argument
     part = args.part
     type = args.type
+    # part = 0
+    # type = "Two_Link_2D"
     if type == 0:
         type = "Rigidbody_2D"
     elif type == 1:
         type = "Two_Link_2D"
     print("Part:", part)
     print("type:", type)
-    env_file = "./Data/S2D/S2D_env_100_rec.npy"
+    env_file = "./Data/S2D/S2D_env_30000_rec.npy"
     if type == "Rigidbody_2D":
         vis = "./fig/S2D/S2D_Rigidbody/S2D_Rigidbody"
-        path_save_file = "./Data/S2D/S2D_Rigidbody_Path_" + str(part)
-        s_g_file = "./Data/S2D/S2D_env_100_pts_4000_Rigidbody.npy"
+        path_save_file = "./Data/S2D/1000env_400pt/S2D_Rigidbody_Path_" + str(part)
+        s_g_file = "./Data/S2D/1000env_400pt/S2D_env_100_pts_4000_Rigidbody.npy"
     if type == "Two_Link_2D":
         vis = "./fig/S2D/S2D_Two_Link/S2D_Two_Link"
-        path_save_file = "./Data/S2D/S2D_Two_Link_Path_"+str(part)
-        s_g_file = "./Data/S2D/S2D_env_100_pts_4000_Two_Link_Path.npy"
-    env_file = "./Data/S2D/S2D_env_100_rec.npy"
-
+        path_save_file = "./Data/S2D/1000env_400pt/S2D_Two_Link_Path_"+str(part)
+        s_g_file = "./Data/S2D/1000env_400pt/S2D_env_100_pts_4000_Two_Link_Path.npy"
     # load env
     rec_envs = np.load(env_file, allow_pickle=True)
-    pl = Plan(type)
+    pl = Plan(type,"RRTstar")
     g_s_g = 0
 
     # generate start and goal
     if g_s_g:
         env_pts = []
-        for i in range(100):
+        for i in range(1000):
             print("Generating start goal, Env:", i)
             rec_env = rec_envs[i, :, :]
             pl.env_rob.load_rec_obs_2D(rec_env)
             pts = []
-            for j in range(4000):
+            for j in range(400):
                 print(i, j)
                 start, goal = pl.pl_ompl.generate_valid_start_goal()
                 start = pl.pl_ompl.convert_ompl_config_to_list_config(start)
@@ -52,10 +52,10 @@ def generate_path_main(args):
     # planning
     paths_all = []
     suc_cnt = 0
-    for i in range(10 * part, 10 * (part + 1)):
+    for i in range(100 * part, 100 * (part + 1)):
         print("Planning Env:", i)
         paths_env = []
-        for j in range(4000):
+        for j in range(400):
             pl.reboot()
             vis_i_j = vis + "_env_" + str(i) + "_pts_" + str(j) + "mpn"
             start = env_pts[i][j][0]
@@ -64,13 +64,14 @@ def generate_path_main(args):
             goal = pl.pl_ompl.conver_list_config_to_ompl_config(goal)
             rec_env = rec_envs[i, :, :]
             pl.env_rob.load_rec_obs_2D(rec_env)
-            solved, path = pl.plan(start=start, goal=goal, vis=vis_i_j, time_lim=0.5, simple=False)
+            solved, path = pl.plan(start=start, goal=goal, vis=None, time_lim=0.5, simple=False)
             if solved and solved.asString() == "Exact solution":
                 suc_cnt += 1
                 paths_env.append(path)
         paths_all.append(paths_env)
-    # np.save(path_save_file, np.array(paths_all))
+    np.save(path_save_file, np.array(paths_all, dtype="object"))
     print("suc_cnt", suc_cnt)
+
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
